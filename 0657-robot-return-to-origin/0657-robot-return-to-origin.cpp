26509dc56c0af7469a2a700e10088c5054448856
Gautam-Singh72 <gautamsinghthakur.2004@gmail.com>
2026-04-05 12:20:34 +0530

    Time: 3 ms (46.4%), Space: 10.4 MB (47.26%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
+    //T.C->O(N) AND S.C->O(1)
     bool judgeCircle(string moves) {
-        int leftMove=0, rightMove=0;
-        int upMove=0, downMove=0;
+        int leftMove=0;
+        int upMove=0;
         for(char ch : moves){
             if(ch=='L') leftMove++;
-            else if(ch=='R')    rightMove++;
+            else if(ch=='R')    leftMove--;
             else if(ch=='U')    upMove++;
-            else    downMove++;
+            else    upMove--;
         }
-        return leftMove==rightMove && upMove==downMove;
+        return leftMove==0 && upMove==0;
     }
 };
