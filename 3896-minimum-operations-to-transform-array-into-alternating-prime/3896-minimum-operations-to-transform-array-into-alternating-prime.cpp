fb9541a194954a8dbe8e66255299854dd562004e
Gautam-Singh72 <gautamsinghthakur.2004@gmail.com>
2026-04-12 00:51:16 +0530

    Time: 220 ms (72.05%), Space: 125.1 MB (42.1%) - LeetHub

--- 3896-minimum-operations-to-transform-array-into-alternating-prime/3896-minimum-operations-to-transform-array-into-alternating-prime.cpp
+++ 3896-minimum-operations-to-transform-array-into-alternating-prime/3896-minimum-operations-to-transform-array-into-alternating-prime.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
-    void sieve(vector<bool>& isPrime){
-        int n=isPrime.size();   //find all prime number less than N for example N=36 it find all prime number less than 36
-        isPrime[0]=isPrime[1]=false;
-        for(int i=2; i*i<n; i++){ 
-            if(isPrime[i]){
-                for(int j=i*i; j<n; j+=i){
-                    isPrime[j]=false;
+    void findPrimeInRange(vector<bool>& prime){
+        prime[0]=prime[1]=false;
+        int n=prime.size();
+        for(int i=2; i*i<n; i++){
+            if(prime[i]){
+                for(int k=i*i; k<n; k+=i){
+                    prime[k]=false;
                 }
             }
         }
     }
     int minOperations(vector<int>& nums) {
-        int maxValue=*max_element(nums.begin(), nums.end());
-        vector<bool> isPrime(maxValue+100, true);
-        sieve(isPrime);
-        int count=0;
+        vector<bool> prime(1e5+100, true);
+        findPrimeInRange(prime);
         int n=nums.size();
+        int m=prime.size();
+        int count=0;
         for(int i=0; i<n; i++){
-            int j=nums[i];
-            if(i%2){
-                while(isPrime[j]){
-                    j++;
-                    count++;
+            if(i%2==0){
+                if(prime[nums[i]]==false){
+                    int j=nums[i];
+                    while(j<m && prime[j]==false){
+                        count++;
+                        j++;
+                    }
                 }
             }else{
-                while(!isPrime[j]){
-                    j++;
-                    count++;
+                if(prime[nums[i]]==true){
+                    int j=nums[i];
+                    while(j<m && prime[j]==true){
+                        count++;
+                        j++;
+                    }
                 }
             }
         }
