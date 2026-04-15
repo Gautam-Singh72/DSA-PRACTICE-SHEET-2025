654cccc3aae6b50e8d2b505237e2ba794d3b7e9b
Gautam-Singh72 <gautamsinghthakur.2004@gmail.com>
2026-04-15 19:32:59 +0530

    Time: 0 ms (100%), Space: 18.4 MB (37.22%) - LeetHub

--- 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.cpp
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int closestTarget(vector<string>& words, string target, int startIndex) {
+        if(words[startIndex]==target)   return 0;
         int n=words.size();
-        if(words[startIndex]==target) return  0;
-        int prev_idx=(startIndex-1+n)%n;
-        int next_idx=(startIndex+1)%n;
+        int prevIndex=(startIndex-1+n)%n;
+        int nextIndex=(startIndex+1)%n;
         int dis=1;
-        while(prev_idx != startIndex && next_idx!=startIndex){
-            if(words[prev_idx]==target)  return dis;
-            if(words[next_idx]==target) return dis;
+        //runs O(N) times 
+        while(prevIndex!=startIndex){
+            if(words[prevIndex]==target || words[nextIndex]==target)    return dis;
 
             dis++;
-            prev_idx=(prev_idx-1+n)%n;
-            next_idx=(next_idx+1)%n;
+            prevIndex=(prevIndex-1+n)%n;
+            nextIndex=(nextIndex+1)%n;
         }
 
         return -1;
