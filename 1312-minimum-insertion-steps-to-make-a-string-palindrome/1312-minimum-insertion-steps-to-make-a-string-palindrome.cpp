4018936b1526b850d825abc43f3baa005ac3a0ba
Gautam-Singh72 <gautamsinghthakur.2004@gmail.com>
2026-04-19 00:43:44 +0530

    Time: 35 ms (48.31%), Space: 31.5 MB (38.94%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> dp;
-    int solve(int i, int j, string &s, string &t){
-        if(i<0 || j<0)  return 0;
-        if(dp[i][j] != -1)  return dp[i][j];
-
-        if(s[i]==t[j])  return dp[i][j]=1+solve(i-1, j-1, s, t);
-
-        int left=solve(i-1, j, s, t);
-        int right=solve(i, j-1, s, t);
-
-        return dp[i][j]=max(left, right);
-    }
     int minInsertions(string s) {
         int n=s.size();
         string t=s;
         reverse(t.begin(), t.end());
-        dp.resize(n, vector<int>(n, -1));
+        dp.resize(n+1, vector<int>(n+1, -1));
 
-        int len=solve(n-1, n-1, s, t);
+        for(int i=0; i<=n; i++){
+            dp[i][0]=0;
+        }
+        for(int j=0; j<=n; j++){
+            dp[0][j]=0;
+        }
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1]; continue;
+                }
+
+                dp[i][j]=max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+
+        int len=dp[n][n];
         int ans=(n-len);
 
         return ans;
