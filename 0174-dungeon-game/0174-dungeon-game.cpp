376bc87a8dd184b9cf9f17434a4ca51c4f07e132
Gautam-Singh72 <gautamsinghthakur.2004@gmail.com>
2026-04-20 00:02:16 +0530

    Time: 3 ms (19%), Space: 13.2 MB (8.01%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.cpp
+++ 0174-dungeon-game/0174-dungeon-game.cpp
@@ -19,10 +19,31 @@ public:
         int res=min(right, down)-dungeon[i][j];
         return dp[i][j]=res<=0 ? 1 : res;
     }
+    //T.C->O(N*M) AND S.C->O(N*M) AUXILARY SPACE RECURSION SPACE REDUCES IN TABULATION
     int calculateMinimumHP(vector<vector<int>>& dungeon) {
         n=dungeon.size();
         m=dungeon[0].size();
-        dp.resize(n, vector<int>(m, -1));
-        return solve(0, 0, dungeon);
+        dp.resize(n+1, vector<int>(m+1));
+        //base case 
+        for(int i=0; i<=n; i++){
+            dp[i][m]=INT_MAX;
+        }
+        for(int j=0; j<=m; j++){
+            dp[n][j]=INT_MAX;
+        }
+        dp[n-1][m-1]=dungeon[n-1][m-1]>0 ? 1 : 1+abs(dungeon[n-1][m-1]);
+
+        //recursive case 
+        for(int i=n-1; i>=0; i--){
+            for(int j=m-1; j>=0; j--){
+                if(i==n-1 && j==m-1)    continue;
+                int right=dp[i][j+1];
+                int down=dp[i+1][j];
+
+                int res=min(right, down)-dungeon[i][j];
+                dp[i][j]=res<=0 ? 1 : res;
+            }
+        }
+        return dp[0][0];
     }
 };
