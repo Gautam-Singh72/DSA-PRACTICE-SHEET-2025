3215346aeb86c7998493f1cec6f3709115119abe
Gautam-Singh72 <gautamsinghthakur.2004@gmail.com>
2026-07-07 14:41:11 +0530

    Time: 4 ms (28.44%), Space: 15.2 MB (30.65%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //custom sorting (ascending by first and desc by second if first is same)
     static bool compare(vector<int> &a, vector<int>& b){
         if(a[0]<b[0])   return true;
         if(a[0]==b[0] && a[1]>b[1])   return true;
@@ -11,17 +12,18 @@ public:
         int n=intervals.size();
         sort(intervals.begin(), intervals.end(), compare); //n*(logn)
 
-        vector<vector<int>> result; //O(n) space 
-        result.push_back(intervals[0]);
+        int lastElementIdx=intervals[0][1]; //O(1) space 
+        int count=1; //non overlapping intervals count
         for(int j=1; j<n; j++){ //O(n)
             int start=intervals[j][0];
             int end=intervals[j][1];
             //start>=result.back()[0] since sorted no need of this condition
-            if(end<=result.back()[1]){
+            if(end<=lastElementIdx){ //overlapped interval
                 continue;
             }
-            result.push_back(intervals[j]);
+            lastElementIdx=intervals[j][1]; //non overlapped interval
+            count++;
         }
-        return result.size();
+        return count;
     }
 };
