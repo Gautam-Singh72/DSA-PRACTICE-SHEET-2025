f9334c165e286ceaf2a5b56c9a683aeb5ec5c0d7
Gautam-Singh72 <gautamsinghthakur.2004@gmail.com>
2026-07-15 18:46:07 +0530

    Time: 298 ms (27.93%), Space: 296.5 MB (21.31%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> dp;
-    int solve(int i, int prev, vector<int>& nums){
-        if(i>=nums.size())  return 0;
-        if(dp[i][prev+1]!=-1)   return dp[i][prev+1];
+    // int solve(int i, int prev, vector<int>& nums){
+    //     if(i>=nums.size())  return 0;
+    //     if(dp[i][prev+1]!=-1)   return dp[i][prev+1];
 
-        int pick=INT_MIN;
-        if(prev==-1 || nums[i]>nums[prev]){
-            pick=1+solve(i+1, i, nums);
-        }
-        int notPick=solve(i+1, prev, nums);
+    //     int pick=INT_MIN;
+    //     if(prev==-1 || nums[i]>nums[prev]){
+    //         pick=1+solve(i+1, i, nums);
+    //     }
+    //     int notPick=solve(i+1, prev, nums);
 
-        return dp[i][prev+1] = max(pick, notPick);
-    }
+    //     return dp[i][prev+1] = max(pick, notPick);
+    // }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        dp.assign(n, vector<int>(n+1, -1));
-        return solve(0, -1, nums);
+        dp.assign(n+1, vector<int>(n+1, -1));
+
+        for(int prev=-1; prev<n; prev++){
+            dp[n][prev+1]=0;
+        }
+
+        for(int i=n-1; i>=0; i--){
+            for(int prev=i-1; prev>=-1; prev--){
+                int pick=INT_MIN;
+                if(prev==-1 || nums[i]>nums[prev]){
+                    pick=1+dp[i+1] [i+1];
+                }
+                int notPick=dp[i+1] [prev+1];
+
+                dp[i][prev+1] = max(pick, notPick);
+            }
+        }
+
+        return dp[0][0];
     }
 };
