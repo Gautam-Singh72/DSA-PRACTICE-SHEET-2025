a305ef30fce99fe8ecf3d3b5a5daadfc9af88940
Gautam-Singh72 <gautamsinghthakur.2004@gmail.com>
2026-07-15 15:07:01 +0530

    Time: 46 ms (77.03%), Space: 47.8 MB (55.74%) - LeetHub

--- 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.cpp
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.cpp
@@ -3,9 +3,12 @@ public:
     int m;
     int n;
     int MOD = 1e9+7;
-    vector<vector<int>> dp;
+    vector<vector<int>> dp; //auxilary space O(m*n)
+
+    //T.C->O(m*n) each cell is visited atmost once
+    //S.C->O(m*n) Aux space and O(m*n) rescursion stack space in worst case
     int dx[4]={-1, 0, 1, 0}; int dy[4]={0, -1, 0, 1};
-    bool isValid(int r, int c){
+    bool isValid(int r, int c){ //O(1)
         if(r<0 || r>=m || c<0 || c>=n){
             return false;
         }
@@ -20,11 +23,11 @@ public:
             int r=i+dx[k];
             int c=j+dy[k];
             if(isValid(r, c) && grid[r][c]>grid[i][j]){
-                count+= solve(r, c, grid) % MOD;
+                count= (0LL + count + solve(r, c, grid)) % MOD;
             }
         }
 
-        return dp[i][j]=count;
+        return dp[i][j]=count; //memoize the result to reduce overlapping sub problems
     }
     int countPaths(vector<vector<int>>& grid) {
         m=grid.size();
@@ -32,7 +35,7 @@ public:
         dp.resize(m, vector<int>(n, -1));
 
         int totalPath=0;
-        for(int i=0; i<m; i++){
+        for(int i=0; i<m; i++){ //O(m*n)
             for(int j=0; j<n; j++){
                 totalPath=(0LL + totalPath + solve(i, j, grid)) % MOD;
             }
