e65066ef1e0566a9488993bddbc7ff55293f89a9
Gautam-Singh72 <gautamsinghthakur.2004@gmail.com>
2026-07-18 13:48:51 +0530

    Time: 5 ms (96.2%), Space: 91.9 MB (74.37%) - LeetHub

--- 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.cpp
+++ 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
+    //T.C->O(m*(log(max(numsDivide[i]))) + n*log(n))
+    //S.C->O(1)
     int minOperations(vector<int>& nums, vector<int>& numsDivide) {
         int n=nums.size();
         int m=numsDivide.size();
         int gcd=numsDivide[0];
+        // m*(log(max(numsDivide[i])))
         for(int i=1; i<m; i++){
-            cout<<gcd<<endl;
             gcd = __gcd(gcd, numsDivide[i]);
         }
 
+        // n*log(n)
         sort(nums.begin(), nums.end());
 
         int count=0;
-        for(int i=0; i<n; i++){
-            cout<<gcd<<" "<<nums[i]<<endl;
+        for(int i=0; i<n; i++){ //O(n)
             if(gcd%nums[i] == 0){
                 return count;   
             }
