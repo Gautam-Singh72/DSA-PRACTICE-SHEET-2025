e2ddd2dc4a01f25dfd0821135bb05cd308d9d1c3
Gautam-Singh72 <gautamsinghthakur.2004@gmail.com>
2026-07-21 23:41:02 +0530

    Time: 0 ms (100%), Space: 9.3 MB (65.86%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        int n=s.size();
+        vector<bool> visited(26, 0);
+        vector<int> lastIndex(26, 0);
+        for(int i=0; i<n; i++){
+            int idx=s[i]-'a';
+            lastIndex[idx]=i;
+        }
+
+        string res="";
+        for(int i=0; i<n; i++){
+            if(!visited[s[i]-'a']){
+                while(res!="" && res.back()>s[i] && lastIndex[res.back()-'a']>i){
+                    visited[res.back()-'a']=false;
+                    res.pop_back();
+
+                }
+                res.push_back(s[i]);
+                visited[s[i]-'a']=true;
+            }
+        }
+
+        return res;
+    }
+};
