9ef901e60f709f57ca81067c2e6f4e4ab1421375
Gautam-Singh72 <gautamsinghthakur.2004@gmail.com>
2026-08-04 15:03:48 +0530

    Time: 2 ms (70.03%), Space: 18 MB (68.57%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,31 +1,39 @@
 class Solution {
 public:
-    static bool compare(pair<string, int>&a, pair<string, int>&b){
-        if(a.second > b.second) return true;
-        if(a.second == b.second && a.first < b.first)   return true;
+    static bool compare(const pair<int, string>& a, const pair<int, string>& b){
+        if(a.first > b.first) return true;
+        if(a.first == b.first && a.second < b.second)   return true;
 
         return false;
     }
     vector<string> topKFrequent(vector<string>& words, int k) {
-        vector<pair<string, int>> res;
         unordered_map<string, int> mp;
 
         for(string &word: words){
-            if(mp.count(word)){
-                int idx=mp[word];
-                res[idx].second+=1;
-                continue;
+            mp[word]++;
+        }
+        priority_queue<pair<int, string>, vector<pair<int, string>>, decltype(&compare)> pq(compare);
+        for(auto it=mp.begin(); it!=mp.end(); it++){
+            if(k){
+                pq.push({it->second, it->first});
+                k--;
+            }else{
+                int freq=it->second;
+                string word=it->first;
+                if(freq > pq.top().first || (freq == pq.top().first && word < pq.top().second)){
+                    pq.pop();
+                    pq.push({freq, word});
+                }
             }
-            res.push_back({word, 1});
-            mp[word]=res.size()-1;
         }
-
-        sort(res.begin(), res.end(), compare);
-        vector<string> temp;
-        for(int i=0; i<k; i++){
-            temp.push_back(res[i].first);
+        vector<string> res;
+        while(!pq.empty()){
+            auto p=pq.top();
+            pq.pop();
+            res.push_back(p.second);
         }
+        reverse(res.begin(), res.end());
 
-        return temp;
+        return res;
     }
 };
