3e581c66e3ef0d67a242101ec7143ac01b19821d
Gautam-Singh72 <gautamsinghthakur.2004@gmail.com>
2026-08-05 12:50:55 +0530

    Time: 19 ms (9.54%), Space: 19.7 MB (15.07%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    vector<vector<vector<int>>> dp;
-    int solve(int l, int r, int player, vector<int>& nums){
+    //T.C-O(n) and S.C-O(n) rec stack space and O(n) auxillary space
+    vector<vector<int>> dp;
+    int solve(int l, int r, vector<int>& piles){
         if(l>r) return 0;
-        if(dp[l][r][player] != -1)  return dp[l][r][player];
+        if(dp[l][r] != -1)  return dp[l][r];
 
-        if(!player){
-            int left_pick=nums[l]+solve(l+1, r, !player, nums);
-            int right_pick=nums[r]+solve(l, r-1, !player, nums);
-            return dp[l][r][player]=max(left_pick, right_pick);
-        }else{
-            int left_pick=nums[l]+solve(l+1, r, !player, nums);
-            int right_pick=nums[r]+solve(l, r-1, !player, nums);
-            return dp[l][r][player]=(left_pick, right_pick);
-        }
-        return -1;
+        int left_pick=piles[l]-solve(l+1, r, piles);
+        int right_pick=piles[r]-solve(l, r-1, piles);
+
+        return dp[l][r]=max(left_pick, right_pick);
     }
     bool stoneGame(vector<int>& piles) {
         int n=piles.size();
-        dp.resize(n, vector<vector<int>>(n, vector<int>(2, -1)));
-        int totalScore=accumulate(piles.begin(), piles.end(), 0);
-        int score1 = solve(0, n-1, 0, piles);
-        int score2=totalScore-score1;
+        dp.resize(n, vector<int>(n, -1));
+        
+        int diff=solve(0, n-1, piles);
 
-        return score1 > score2;
+        return diff > 0;
     }
 };
