20e6ccf856fb5499547d4ca4a5cd0826f82d0f07
Gautam-Singh72 <gautamsinghthakur.2004@gmail.com>
2026-08-17 15:28:52 +0530

    Time: 9 ms (6.65%), Space: 29.3 MB (8.14%) - LeetHub

--- 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.cpp
+++ 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool isIncrease(vector<int>& v){
-
         for(int i=1; i<v.size(); i++){
             if(v[i] < v[i-1]) return false;
         }
@@ -10,16 +9,14 @@ public:
     int minimumPairRemoval(vector<int>& nums) {
         int n=nums.size();
         int count=0;
-        while(!isIncrease(nums) && nums.size()>1){
+        while(!isIncrease(nums)){
             count++;
-            int minSum=INT_MAX;
-            int idx1=-1, idx2=-1;
+            int minSum=INT_MAX; int idx1=-1;
             for(int i=1; i<nums.size(); i++){
                 int sum=nums[i]+nums[i-1];
                 if(minSum > sum){
                     minSum=sum;
                     idx1=i-1;
-                    idx2=i;
                 }
             }
             vector<int> temp;
@@ -33,7 +30,7 @@ public:
                     i++;
                 }
             }
-            nums.clear();
+            // nums.clear();
             nums=temp;
         }
         return count;
