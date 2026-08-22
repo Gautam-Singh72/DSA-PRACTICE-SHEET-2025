9a8a4e194be71f61ca920da3a9116fea701c8122
Gautam-Singh72 <gautamsinghthakur.2004@gmail.com>
2026-08-23 00:40:41 +0530

    Time: 3 ms (85.63%), Space: 8.5 MB (91.32%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
+    //smart approach t.c-O(n^2) ans s.c-O(1)
+    int countPalindrome(int i, int j, string& s){
+        int n=s.size();
+        int count=0;
+        while(i>=0 && j<n && s[i]==s[j]){
+            count++;
+            i--; j++;
+        }
+
+        return count;
+    }
     int countSubstrings(string s) {
         int n=s.size();
 
-        vector<vector<int>> t(n, vector<int>(n, false));
         int count=0;
-        for(int L=1; L<=n; L++){
-            for(int i=0; i+L-1<n; i++){
-                int j=i+L-1;
-                if(s[i]==s[j] && (L<=2 || t[i+1][j-1])){
-                    t[i][j]=true;
-                }
-
-                if(t[i][j]) count++;
-            }
+        for(int i=0; i<n; i++){
+            count+=countPalindrome(i, i, s);
+            count+=countPalindrome(i, i+1, s);
         }
 
         return count;
