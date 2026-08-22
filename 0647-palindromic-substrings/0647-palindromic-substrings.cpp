89c1c0b0831428a76cde504e51ec31af98b2dc99
Gautam-Singh72 <gautamsinghthakur.2004@gmail.com>
2026-08-23 00:02:39 +0530

    Time: 540 ms (11.94%), Space: 34.5 MB (18.3%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
+    vector<vector<int>> t;
     bool isPalindrome(int i, int j, string& s){
+        if(t[i][j] != -1)   return t[i][j];
 
+        int x=i, y=j;
         while(i<j){
-            if(s[i] != s[j])    return false;
+            if(s[i] != s[j])    return t[i][j]=false;
+
             i++; j--;
         }
 
-        return true;
+        return t[x][y]=true;
     }
     int countSubstrings(string s) {
         int n=s.size();
 
+        t.resize(n, vector<int>(n, -1));
         int count=0;
         for(int i=0; i<n; i++){
             for(int j=i; j<n; j++){
