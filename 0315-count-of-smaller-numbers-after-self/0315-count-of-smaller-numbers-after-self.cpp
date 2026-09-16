55dd09c4d808aa6b3b79d8dfdcf1bac799ff04ff
Gautam-Singh72 <gautamsinghthakur.2004@gmail.com>
2026-09-16 15:59:58 +0530

    Time: 334 ms (59.85%), Space: 252.5 MB (40.29%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
@@ -1,44 +1,45 @@
 class Solution {
 public:
-    void update(int i, int l, int r, int idx, int val, vector<int>& st){
-        if(l==r){
-            st[i]+=val;
-            return;
+    void merge(int start, int mid, int end, vector<int>& ans, vector<pair<int, int>>& numsPair){
+        vector<pair<int, int>> temp(end-start+1);
+        int i=start, j=mid+1;
+        int k=0;
+        while(i<=mid && j<=end){
+            if(numsPair[i].first > numsPair[j].first){
+                ans[numsPair[i].second]+=(end-j+1);
+                temp[k++]=numsPair[i++];
+            }else{
+                temp[k++]=numsPair[j++];
+            }
         }
 
-        int mid=l+(r-l)/2;
-        if(idx<=mid){
-            update(2*i+1, l, mid, idx, val, st);
-        }else{
-            update(2*i+2, mid+1, r, idx, val, st);
+        while(i<=mid){
+            temp[k++]=numsPair[i++];
         }
-        st[i]=st[2*i+1]+st[2*i+2];
+        while(j<=end){
+            temp[k++]=numsPair[j++];
+        }
+        i=start; k=0;
+        while(i<=end){
+            numsPair[i++]=temp[k++];
+        }
     }
-    
-    int sum(int i, int l, int r, int start, int end, vector<int>& st){
-        if(r<start || l>end)    return 0;
+    void mergeSort(int start, int end, vector<int>& ans, vector<pair<int, int>>& numsPair){
+        if(start>=end)   return;
 
-        if(l>=start && r<=end)  return st[i];
-
-        int mid=l+(r-l)/2;
-        return sum(2*i+1, l, mid, start, end, st)+sum(2*i+2, mid+1, r, start, end, st);
+        int mid=start+(end-start)/2;
+        mergeSort(start, mid, ans, numsPair);
+        mergeSort(mid+1, end, ans, numsPair);
+        merge(start, mid, end, ans, numsPair);
     }
     vector<int> countSmaller(vector<int>& nums) {
         int n=nums.size();
-        vector<int> st(4*n, 0);
-
-        vector<pair<int, int>> v;
-        for(int i=0; i<n; i++){
-            v.push_back({nums[i], i});
-        }
-
-        sort(v.begin(), v.end());
-
         vector<int> ans(n);
-        for(auto& [elem, index]: v){
-            update(0, 0, n-1, index, 1, st);
-            ans[index]=sum(0, 0, n-1, index+1, n-1, st);
+        vector<pair<int, int>> numsPair;
+        for(int i=0; i<n; i++){
+            numsPair.push_back({nums[i], i});
         }
+        mergeSort(0, n-1, ans, numsPair);
 
         return ans;
     }
