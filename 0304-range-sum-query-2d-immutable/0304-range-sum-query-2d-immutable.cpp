215736aedc976ec5586afe36292bb1270cced7dc
Gautam-Singh72 <gautamsinghthakur.2004@gmail.com>
2026-09-21 01:57:52 +0530

    Time: 18 ms (81.93%), Space: 149.2 MB (59.73%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -1,27 +1,30 @@
 class NumMatrix {
 public:
     //T.C->O(m) and S.C->O(m*n)
-    vector<vector<int>> prefixSum;
-    NumMatrix(vector<vector<int>>& matrix) { //O(m*n)
+    vector<vector<int>> pref;
+    NumMatrix(vector<vector<int>>& matrix) { //O(m*n) precomputation
         int m=matrix.size();
         int n=matrix[0].size();
-        prefixSum.resize(m, vector<int>(n));
-        for(int i=0; i<m; i++){
-            prefixSum[i][0]=matrix[i][0];
-            for(int j=1; j<n; j++){
-                prefixSum[i][j]=prefixSum[i][j-1]+matrix[i][j];
+        pref.resize(m+1, vector<int>(n+1));
+        for(int i=1; i<=m; i++){
+            // prefixSum[i][0]=matrix[i][0];
+            for(int j=1; j<=n; j++){
+                pref[i][j]=matrix[i-1][j-1] + pref[i-1][j] + pref[i][j-1] - pref[i-1][j-1];
             }
         }
     }
     
-    int sumRegion(int row1, int col1, int row2, int col2) { //O(m)
-        int sum=0;
-        while(row1 <= row2){
-            sum+=(col1==0) ? prefixSum[row1][col2] : prefixSum[row1][col2]-prefixSum[row1][col1-1];
-            row1++;
-        }
+    int sumRegion(int row1, int col1, int row2, int col2) { //O(1)
+        //converting query in 1 base indexing
+        int x1=row1+1, y1=col1+1;
+        int x2=row2+1, y2=col2+1;
 
-        return sum;
+        int incl1=pref[x2][y2];
+        int excl1=pref[x1-1][y2];
+        int excl2=pref[x2][y1-1];
+        int incl2=pref[x1-1][y1-1];
+
+        return incl1 - excl1 - excl2 + incl2;
     }
 };
 
